f85b09f8810f2014d44607ca8f1708b621d2b8f8
Jann Poppinga <jpoppinga@jacobs-university.de>
2011-01-27 16:45:12 +0100

    C list version working

--- C/letterMatrix.c
+++ C/letterMatrix.c
@@ -142,10 +142,6 @@ Node* dict2tree(const char* dictFileName) {
     root=root->sibling;
     free(prev);
 
-    int show=0;
-    if( show ) {
-        printTreeRec(0,root);
-    }
     return root;
 }
 
@@ -207,7 +203,7 @@ int wordsInMatrix(Node* root, Matrix* matrix) {
             }
             if( sibling ) {
                 frame=malloc(sizeof(Frame));
-                printf("initial node %p: (%d,%d) c=%c\n",frame,j,i,c);
+//                printf("initial node %p (%d,%d) c=%c\n",frame,j,i,c);
                 frame->row=j;
                 frame->col=i;
                 frame->word=calloc(2,sizeof(char));
@@ -225,19 +221,26 @@ int wordsInMatrix(Node* root, Matrix* matrix) {
             }
         }
     }
+    if( last ) {
+        last->next=NULL;
+    }
+    frame=first;
     int row,col;
     int steps[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
-    for(i=0;i<4;++i) {
-        printf("(%d,%d)\n",steps[i][0],steps[i][1]);
-    }
-    while( first!=last ) {
+    while( first ) {
         //        c=first->word[first->length-1];
         c=first->node->c;
-        //        printf("node %p with c=%c, length %d, (%d,%d)\n",first,first->node->c,first->length,first->row,first->col);
+//        printf("node %p with c=%c, length %d, (%d,%d)\n",first,first->node->c,first->length,first->row,first->col);
 
         if( (child=find_child(first->node, '\n')) ) {
             printf("found word '%s'\n",first->word);
         }
+/*         if( strcmp(first->word,"eat")==0 ) { */
+/*             printf("@ eat!\n"); */
+/*         } */
+/*         if( strcmp(first->word,"eata")==0 ) { */
+/*             printf("@ eata!\n"); */
+/*         } */
         for(i=0;i<4;++i) {
             row=first->row+steps[i][0];
             col=first->col+steps[i][1];
@@ -247,78 +250,20 @@ int wordsInMatrix(Node* root, Matrix* matrix) {
                 child=find_child(first->node, c);
                 if( child ) {
                     frame=malloc(sizeof(Frame));
+//                    printf("creating node %p\n",frame);
                     frame->row=row;
                     frame->col=col;
                     frame->length=first->length+1;
-                    frame->word=malloc(sizeof(char)*(frame->length+1));
+                    frame->word=calloc((frame->length+1),sizeof(char));
                     strncpy(frame->word, first->word, first->length);
                     frame->word[first->length]=c;
                     frame->word[frame->length]=0;
                     frame->node=child;
                     last->next=frame;
+                    frame->next=NULL;
                     last=frame;
                 }
             }
-/*         if( 1 ) { */
-/*             //north */
-/*             if( first->row>0 ) { */
-/*                 row=first->row-1; */
-/*                 col=first->col; */
-/*                 c=matrix->matrix[row][col]; */
-/*                 child=find_child(first->node, c); */
-/*                 if( child ) { */
-/*                     frame=malloc(sizeof(Frame)); */
-/*                     frame->row=row; */
-/*                     frame->col=col; */
-/*                     frame->length=first->length+1; */
-/*                     frame->word=malloc(sizeof(char)*(frame->length+1)); */
-/*                     strncpy(frame->word, first->word, first->length); */
-/*                     frame->word[first->length]=c; */
-/*                     frame->word[frame->length]=0; */
-/*                     frame->node=child; */
-/*                     last->next=frame; */
-/*                     last=frame; */
-/*                 } */
-/*             } */
-/*             //south */
-/*             if( first->row<matrix->height-1 ) { */
-/*                 frame=malloc(sizeof(Frame)); */
-/*                 frame->row=first->row+1; */
-/*                 frame->col=first->col; */
-/*                 frame->length=first->length+1; */
-/*                 frame->word=malloc(sizeof(char)*(frame->length+1)); */
-/*                 frame->word[first->length]=c; */
-/*                 frame->word[frame->length]=0; */
-/*                 frame->node=child; */
-/*                 last->next=frame; */
-/*                 last=frame; */
-/*             } */
-/*             //west */
-/*             if( first->col>0 ) { */
-/*                 frame=malloc(sizeof(Frame)); */
-/*                 frame->row=first->row; */
-/*                 frame->col=first->col-1; */
-/*                 frame->length=first->length+1; */
-/*                 frame->word=malloc(sizeof(char)*(frame->length+1)); */
-/*                 frame->word[first->length]=c; */
-/*                 frame->word[frame->length]=0; */
-/*                 frame->node=child; */
-/*                 last->next=frame; */
-/*                 last=frame; */
-/*             } */
-/*             //east */
-/*             if( first->col<matrix->width-1 ) { */
-/*                 frame=malloc(sizeof(Frame)); */
-/*                 frame->row=first->row; */
-/*                 frame->col=first->col+1; */
-/*                 frame->length=first->length+1; */
-/*                 frame->word=malloc(sizeof(char)*(frame->length+1)); */
-/*                 frame->word[first->length]=c; */
-/*                 frame->word[frame->length]=0; */
-/*                 frame->node=child; */
-/*                 last->next=frame; */
-/*                 last=frame; */
-/*             } */
         }
         frame=(Frame*)first->next;
         free(first->word);
@@ -343,9 +288,14 @@ int main(int argc, const char** argv) {
     Node* root=dict2tree(dictFileName);
     Matrix* matrix=parseMatrix(matrixFileName);
 
+    int show=0;
+    if( show ) {
+        printTreeRec(0,root);
+    }
     wordsInMatrix(root,matrix);
-
-
+    if( show ) {
+        printTreeRec(0,root);
+    }
     int i;
     for( i=3;i<argc;++i ) {
         printf("'%s' %s in tree\n",argv[i],(word_in_tree(root,argv[i])?"is":"is not"));
